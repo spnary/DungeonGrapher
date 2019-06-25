2efdbb8bfd0bdee2db17b8d3965d606ae69c3b82
Stephen Nary <spnary@gmail.com>
2019-06-25 19:40:12 -0400

    Initial Commit

---
+++ DungeonGrapher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DungeonGrapher
+//
+//  Created by Stephen Nary on 6/25/19.
+//  Copyright © 2019 Stephen Nary. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
